e74d94f15192ae65aef54dcf9411bb682f9b3443
Jakub Kuniszewski <you@example.com>
2022-09-01 20:06:26 +0200

    Improve gallery slide logic

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,13 @@
 const galleries = document.getElementsByClassName('gallery');
 const galleryContents = Array.from(galleries).map(x => x.getElementsByClassName('gallery__content')[0]);
 
-let galleryWidth = galleries[0].clientWidth;
+let galleryWidths = galleryContents.map(x => x.scrollWidth);
+
+
 let elementWidth = galleryContents[0].getElementsByClassName('gallery-item')[0].clientWidth;
 addEventListener('resize', () => {
-    galleryWidth = galleries[0].clientWidth;
+    galleryWidths = galleryContents.map(x => x.scrollWidth);
+
     elementWidth = galleryContents[0].getElementsByClassName('gallery-item')[0].clientWidth;
     for(let i = 0;i < galleryContents.length; i++) 
         galleryContents[i].style.left = '0px';
@@ -13,7 +16,14 @@ addEventListener('resize', () => {
 
 const translateGallery = (direction, i) => {
     const left = +getComputedStyle(galleryContents[i]).getPropertyValue('left').split('px')[0];
-    let newLeftRaw = (left + (elementWidth) * direction);
+    let newLeftRaw = Math.trunc(left + (elementWidth + 32) * direction);
+
+    if(newLeftRaw > 0)
+        newLeftRaw = 0;
+
+    if(newLeftRaw < -(galleryWidths[i] - galleries[0].clientWidth))
+        newLeftRaw = -(galleryWidths[i] - galleries[0].clientWidth);
+
     galleryContents[i].style.left = newLeftRaw + 'px';
 }
 
